67cce066833d89ffa873565f6eb235fbe3cf309e
Plag-509-5 <camav1260@gmail.com>
2026-07-06 00:10:54 -0400

    Change default PORT from 3001 to 10000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 let code = require('./pair'); 
 
