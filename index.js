8d72fa6452ff060ea24345d02eaff0004bb5e0e7
PLAG_TECH <camav1260@gmail.com>
2026-08-14 16:52:47 -0400

    Change server port from 10000 to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 let code = require('./pair'); 
 
